5cac7a1c7b8f3ade2fe992f16af743e7f3b7dcd4
Rocotton <Robert.Cotton@labor.idaho.gov>
2016-08-22 12:48:50 -0600

    Initial project creation

---
+++ src/TfsMigrator/Data/UserMapping.cs
@@ -0,0 +1,8 @@
+﻿namespace TfsMigrator.Data
+{
+    public class UserMapping
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+    }
+}
---
+++ src/TfsMigrator/Data/WorkItemMigration.cs
@@ -0,0 +1,8 @@
+﻿namespace TfsMigrator.Data
+{
+    public class WorkItemMigration
+    {
+        public int MigratedId { get; set; }
+        public int OriginalId { get; set; }
+    }
+}
